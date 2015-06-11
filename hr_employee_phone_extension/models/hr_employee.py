ff8298a7f6f67f148bc52bc423a7c364a3c4f261
Charbel Jacquin <charbel.jacquin@camptocamp.com>
2015-06-11 18:19:55 +0200

    adjust code structure to OCA conventions

--- hr_employee_phone_extension/models/hr_employee.py
+++ hr_employee_phone_extension/models/hr_employee.py
@@ -1,13 +1,34 @@
-# copyright 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
-from odoo import models, fields
+from openerp.osv import fields, orm
 
 
-class HrEmployee(models.Model):
+class hr_employee(orm.Model):
     _inherit = 'hr.employee'
 
-    internal_number = fields.Char('Internal Number',
-                                  help='Internal phone number.')
-    short_number = fields.Char('Short Number',
-                               help='Short phone number.')
+    _columns = {
+        'internal_number': fields.char('Internal Number', size=20,
+                                       help='Internal phone number.'),
+        'short_number': fields.char('Short Number', size=20,
+                                    help='Short phone number.'),
+    }
