2c73ca03e8072de614d68aa650f88cae3ec69cab
Salton Massally <salton.massally@gmail.com>
2015-08-25 09:36:38 +0000

    hr_employee_id: Port to V8
    Initial Attempt of a port to V8
    Some improvements made are as follows:
    * Ability so choose ID generation method
    * Sequence vs Random
    
    hr_employee_id: code linting and copyright removals
    
    apply suggeste changes
    * Use identification_id provided by hr addon rather than employee_no
    * use new api in configuration
    * pep8 changes
    
    Update res_company.py
    
    make fields assignable from res_company
    
    add tests
    
    Remove duplicate license  key
    
    import openerp.exception.Warning as UserWarning
    
    [FIX] import Warning as UserWarning

--- hr_employee_id/__init__.py
+++ hr_employee_id/__init__.py
@@ -1,13 +1,11 @@
 
 #
-#
 #    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
-#    All Rights Reserved.
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,11 +15,5 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#
-
-<<<<<<< HEAD
-import hr_employee_id
->>>>>>> Employee and Contract Management, Attendance and Scheduling, Payroll Management modules
-=======
-from . import hr_employee_id
->>>>>>> [FIX] PEP8
+##############################################################################
+from . import models
--- hr_employee_id/__openerp__.py
+++ hr_employee_id/__openerp__.py
@@ -1,13 +1,12 @@
-# -*- coding:utf-8 -*-
-#
+# -*- coding: utf-8 -*-
+##############################################################################
 #
 #    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
-#    All Rights Reserved.
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,27 +16,24 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#
-
+##############################################################################
 {
     'name': 'Employee ID',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
     'category': 'Generic Modules/Human Resources',
-    'description': """
-Employee Identification Numbers
-===============================
-Company wide unique employee ID
-    """,
-    'author': "Michael Telahun Makonnen <mmakonnen@gmail.com>,Odoo Community Association (OCA)",
+    'author': 'Michael Telahun Makonnen, '
+              'Odoo Community Association (OCA)',
     'website': 'http://miketelahun.wordpress.com',
     'license': 'AGPL-3',
     'depends': [
         'hr',
     ],
     'data': [
-        'hr_view.xml',
-    ],
-    'test': [
+        'views/res_config_views.xml',
+        'views/res_company.xml',
+        'views/hr_employee_views.xml',
+        'data/hr_employee_sequence.xml',
     ],
     'installable': True,
 }
