216fbcdea5ae5c84dcc967598dbbc7ccb2576e30
antonio <espo-tony@hotmail.it>
2016-11-01 12:39:56 +0100

    [MIG] hr_holidays_leave_auto_approve: Migrated to 10.0

--- hr_holidays_leave_auto_approve/__manifest__.py
+++ hr_holidays_leave_auto_approve/__manifest__.py
@@ -1,12 +1,13 @@
-# Copyright 2016 Onestein (<http://www.onestein.eu>)
+# -*- coding: utf-8 -*-
+# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Auto Approve Leaves',
-    'version': '11.0.1.0.1',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'summary': '''Leave type for auto approval of Leaves''',
-    'author': 'Onestein, Odoo Community Association (OCA)',
+    'author': 'ONESTEiN BV, Odoo Community Association (OCA)',
     'website': 'http://www.onestein.eu',
     'category': 'Human Resources',
     'depends': [
