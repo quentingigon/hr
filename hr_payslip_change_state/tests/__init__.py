34aafd7eb44744efb7e2f87fa1cd3cc50d44646d
Aarón Henríquez <ahenriquez@eficent.com>
2016-12-05 14:02:10 +0100

    8.0 hr payslip change state (#249)
    * [ADD] hr_payslip_change_state
    
    * [FIX] Readme was incorrect
    
    * [FIX] Comments at https://github.com/Eficent/eficent-odoo-addons/pull/18
    
    * [FIX] Change texts in wizard as they are in form buttons
    
    * [FIX] style
    
    *  [FIX]Migrate to new api, add test and fix style
    
    * [ADD] travis configuration
    
    *  [FIX]  typo in travis file
    
    *  [FIX] style
    
    *  [FIX] style
    
    *  [FIX] style
    
    *  [FIX] travis file
    
    * Revert " [FIX] travis file"
    
    This reverts commit db7968579e0c60f685dd7799b9b1f13b7e22976c.
    
    *  [FIX] travis file
    
    * Revert " [FIX] travis file"
    
    This reverts commit f2431592de21dd40fe3150f73ee283db9b65f483.
    
    *  [DEL] travis file
    
    *  [FIX] travis comments for  commit e0138a5
    
    *  [FIX] Comments by @andhit-r on pull request #249
    
    * [ADD] hr_payslip_change_state
    
    * [FIX] Readme was incorrect
    
    * [FIX] Comments at https://github.com/Eficent/eficent-odoo-addons/pull/18
    
    * [FIX] Change texts in wizard as they are in form buttons
    
    * [FIX] style
    
    *  [FIX]Migrate to new api, add test and fix style
    
    *  [FIX] style
    
    *  [FIX] style
    
    *  [FIX] style
    
    *  [FIX] travis comments for  commit e0138a5
    
    *  [FIX] Comments by @andhit-r on pull request #249
    
    * Fix travis. Rebase upstream. Fix unittest
    
    *  [UPT] Add contributor. Edit description in readme.
    
    *  [FIX] class name

--- hr_payslip_change_state/tests/__init__.py
+++ hr_payslip_change_state/tests/__init__.py
@@ -1,1 +1,4 @@
+# © 2016 - Eficent http://www.eficent.com/
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from . import test_hr_payslip_change_state
--- hr_payslip_change_state/wizard/__init__.py
+++ hr_payslip_change_state/wizard/__init__.py
@@ -1,1 +1,3 @@
+# © 2016 - Eficent http://www.eficent.com/
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import hr_payslip_change_state
